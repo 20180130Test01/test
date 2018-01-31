e03cceb6e58f1e4df23a189b5358b1c1ddff5701
杜彬 <1058919265@qq.com>
2018-01-31 10:39:53 +0800

    002

---
+++ Test1/Assets/Script/UIcontral.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIcontral : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(1);
+        }
+	}
+    public void Click()
+    {
+        Debug.LogError("Button");
+    }
+}
